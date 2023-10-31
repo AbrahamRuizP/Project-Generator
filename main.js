f6ca5a96e2244f3a9fed9c8503226e55778251c9
Abraham Ruiz <habramruizpestana@gmail.com>
2023-10-31 02:55:02 +0100

    updating .js file

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 
-<<<<<<< HEAD
 const cb_application = document.getElementById("application");
 
 cb_application.checked;
 console.log(cb_application.checked);
-=======
->>>>>>> 10abad22a99b8d667de0d555e6c7fd664b54dd34
