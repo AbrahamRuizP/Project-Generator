24de13145f16592ac43f2cdaa87f689005a9a65b
Abraham Ruiz <habramruizpestana@gmail.com>
2023-10-31 03:18:32 +0100

    updating .js and .html files

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 
+const borrar = document.getElementById("borrar");
 const cb_application = document.getElementById("application");
+const cb_db = document.getElementById("db");
+const cb_domain = document.getElementById("domain");
+const cb_extension = document.getElementById("extension");
+const cb_graph = document.getElementById("graph");
+const cb_infrastructure = document.getElementById("infrastructure");
+const cb_server = document.getElementById("server");
+const db_test = document.getElementById("test");
 
+
+borrar.innerHTML = `Is checked is: ${cb_application.checked}`;
 cb_application.checked;
-console.log(cb_application.checked);
+
