8776fdad3ed09140428f9e2080bc3bd1453c72a7
Abraham Ruiz <habramruizpestana@gmail.com>
2023-11-07 01:23:09 +0100

    updating html and js files

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const borrar = document.getElementById("borrar");
+
+// Parent checkboxes
 const cb_application = document.getElementById("application");
 const cb_db = document.getElementById("db");
 const cb_domain = document.getElementById("domain");
@@ -6,8 +7,28 @@ const cb_extension = document.getElementById("extension");
 const cb_graph = document.getElementById("graph");
 const cb_infrastructure = document.getElementById("infrastructure");
 const cb_server = document.getElementById("server");
-const db_test = document.getElementById("test");
+const cb_test = document.getElementById("test");
 
+// Child checkboxes
+const childs_application = document.getElementById("childs_application");
+const childs_db = document.getElementById("childs_db");
+const childs_domain = document.getElementById("childs_domain");
+const childs_extension = document.getElementById("childs_extension");
+const childs_graph = document.getElementById("childs_graph");
+const childs_infrastructure = document.getElementById("childs_infrastructure");
+const childs_server = document.getElementById("childs_server");
+const childs_test = document.getElementById("childs_test");
+
+const family = {
+    "application": [cb_application, childs_application],
+    "db": [cb_db, childs_db],
+    "domain": [cb_domain, childs_domain],
+    "extension": [cb_extension, childs_extension],
+    "graph": [cb_graph, childs_graph],
+    "infrastructure": [cb_infrastructure, childs_infrastructure],
+    "server": [cb_server, childs_server],
+    "test": [cb_test, childs_test]
+}
 
 // Starting Page Logic 
 const starting_page = document.querySelector(".Starting-Page");
@@ -18,21 +39,22 @@ starting_page.addEventListener("click", (e) => {
         const project_type = document.querySelector("#Project-Type-Selector").value; 
         
         //hiding starting page
-        const starting_page = document.querySelector(".Starting-Page").style.display = "none";
+        starting_page.style.display = "none";
 
         //Showing Project Creation Section
         display_section(project_type);
     }
 })
 
-
-function display_section(project_type){
+function display_section(project_type) {
     const section = document.querySelector(`.${project_type}`)
     section.style.display = "block"
 }
 
-
-// Abraham testing some weard stuff
-
-//borrar.innerHTML = `Is checked is: ${cb_application.checked}`;
-//cb_application.checked;
+const checkStatus = ( id ) => {
+    if (family[id][0].checked !== true) {
+        family[id][1].style.display = 'none';
+    } else {
+        family[id][1].style.display = 'block';
+    }
+}
